9d3f7e7740f2933e86810d88b73c0731b13fc289
Ernesto Boca <boca.ernesto13@gmail.com>
2023-09-18 06:57:09 -0700

    Printf for task 0

---
+++ printf.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * _printf - produces output according to a format.
+ * @format: is a character string
+ * Return: the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	int i = 0, cont = 0;
+	char *str, c;
+	va_list args;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			cont++;
+		}
+		else
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				c = va_arg(args, int);
+				_putchar(c);
+				cont++;
+			} else if (format[i] == 's')
+			{
+				str = va_arg(args, char *);
+				_puts(str);
+				cont += _strlen(str);
+			} else if (format[i] == '%')
+			{
+				_putchar('%');
+				cont++;
+			}
+		}
+		i++;
+	}
+
+	va_end(args);
+	return (cont);
+}
